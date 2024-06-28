751da971738ce48118537c5273a437347e471e8c
akashbiswas0 <akash8860biswas@gmail.com>
2024-06-28 22:52:02 +0530

    utils added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
+import { BenefitsSection } from "@/components/landing/layout/sections/benefits";
+import { FeaturesSection } from "@/components/landing/layout/sections/features";
+import { HeroSection } from "@/components/landing/layout/sections/hero";
+import { ServicesSection } from "@/components/landing/layout/sections/services";
 export default function Home() {
   return (
     <main className="bg-[#111111] flex min-h-screen flex-col items-center justify-between">
-      
+      <HeroSection />
+      <BenefitsSection />
+      <FeaturesSection />
+      <ServicesSection />
     </main>
   );
 }
